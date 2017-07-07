ee3af4e50fd485c7c5376aa3bdbbafbf0b06e410
resilientcoders6 <arnold.pires92@gmail.com>
2017-07-07 19:34:39 -0400

    made bricks appear/ made bricks smaller. Made ball smaller

--- js/main.js
+++ js/main.js
@@ -1,14 +1,83 @@
+//Gives ability to animate on the canvas
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
+//Defines the ball and collision
 var x = canvas.width/2;
 var y = canvas.height-30;
 var dx = 2;
 var dy = -2;
-var ballRadius = 10;
+var ballRadius = 5;
 
+//Defining the paddle
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width-paddleWidth)/2;
+
+//Defining the users ability to control the paddle
+var rightPressed = false;
+var leftPressed = false;
+
+//Bricks and brick loop: Defined placement of bricks
+var brickRowCount = 3;
+var brickColumnCount = 5;
+var brickWidth = 40;
+var brickHeight = 10;
+var brickPadding = 10;
+var brickOffsetTop = 10;
+var brickOffsetLeft = 30;
+
+//Creates more than one brick/ adds breaking collision
+var bricks = [];
+for(c=0; c<brickColumnCount; c++) {
+    bricks[c] = [];
+    for(r=0; r<brickRowCount; r++) {
+        bricks[c][r] = { x: 0, y: 0 };
+    }
+}
+
+//Draws the bricks on the screen
+function drawBricks() {
+    for(c=0; c<brickColumnCount; c++) {
+        for(r=0; r<brickRowCount; r++) {
+            var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+            var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+            bricks[c][r].x = brickX;
+            bricks[c][r].y = brickY;
+            ctx.beginPath();
+            ctx.rect(brickX, brickY, brickWidth, brickHeight);
+            ctx.fillStyle = "#0095DD";
+            ctx.fill();
+            ctx.closePath();
+        }
+    }
+}
+
+//Buttons used to move the paddle
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+//The logic of being able to move the paddle from left to right
+function keyDownHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = true;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = true;
+    }
+}
+function keyUpHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = false;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = false;
+    }
+}
+
+
+//What the ball should look like and where should it be placed
 function drawBall() {
-  //Green ball shape and position
   ctx.beginPath();
   ctx.arc(x, y, ballRadius, 0, Math.PI*2);
   ctx.fillStyle = "green";
@@ -16,19 +85,51 @@ function drawBall() {
   ctx.closePath();
 }
 
+//collision to the borders
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBricks();
   drawBall();
+  drawPaddle();
 
   if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
     dx = -dx;
   }
-  if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+  //If the ball hits the bottom, the game will trigger a "GAME OVER" popup box
+  if(y + dy < ballRadius) {
     dy = -dy;
+  }
+  else if(y + dy > canvas.height-ballRadius) {
+      if(x > paddleX && x < paddleX + paddleWidth) {
+        if(y= y-paddleHeight){
+          dy = -dy ;
+                      }
+      }
+      else {
+        alert("GAME OVER");
+        document.location.reload();
+      }
+  }
+
+  if(rightPressed && paddleX < canvas.width-paddleWidth) {
+    paddleX += 7;
+  }
+  else if (leftPressed && paddleX > 0) {
+    paddleX -= 7;
   }
 
   x += dx;
   y += dy;
 }
 
-setInterval(draw, 10);
+//Creating the paddle on the screen
+function drawPaddle() {
+  ctx.beginPath();
+  ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+}
+
+//how fast the ball is moving
+setInterval(draw, 15);
