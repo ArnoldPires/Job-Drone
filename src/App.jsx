84249b280e46c3b0ea76b1b1870320df71d81742
ArnoldPires <arnold.pires92@gmail.com>
2023-12-04 21:26:53 -0500

    Setting up some of the react routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
 import React from 'react'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import {
+  HomeLayout, Landing, Register, Login, DashboardLayout, Error,
+} from './pages';
 
-function App() {
-  return (
-    <div>
-      <h1>Job Drone!</h1>
-    </div>
-  )
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: 'Register',
+        element: <Register />,
+      },
+      {
+        path: 'Login',
+        element: <Login />,
+      },
+      {
+        path: 'Dashboard',
+        element: <DashboardLayout />,
+      },
+    ]
+  },
+])
+
+const App = () => {
+  return <RouterProvider router={router} />;
 }
 
 export default App
